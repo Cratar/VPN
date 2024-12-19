52dda2fba2a810b3f895179fa1a6f704157472ea
Cratar <fateevn935@gmail.com>
2024-12-19 23:03:17 +0300

    Start

--- VPN/main_win.h
+++ VPN/main_win.h
@@ -35,11 +35,19 @@ namespace VPN {
 			}
 		}
 
+	private: System::Windows::Forms::PictureBox^ main_photo;
+
+
+	private:
+
+	private: System::ComponentModel::IContainer^ components;
+	protected:
+
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,23 +56,55 @@ namespace VPN {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(main_win::typeid));
+			this->main_photo = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->main_photo))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// main_photo
+			// 
+			this->main_photo->BackColor = System::Drawing::Color::White;
+			this->main_photo->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"main_photo.BackgroundImage")));
+			this->main_photo->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->main_photo->Location = System::Drawing::Point(-1, -2);
+			this->main_photo->Name = L"main_photo";
+			this->main_photo->Size = System::Drawing::Size(460, 263);
+			this->main_photo->TabIndex = 1;
+			this->main_photo->TabStop = false;
+			this->main_photo->WaitOnLoad = true;
+			this->main_photo->Click += gcnew System::EventHandler(this, &main_win::main_photo_Click);
 			// 
 			// main_win
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->AutoValidate = System::Windows::Forms::AutoValidate::EnablePreventFocusChange;
+			this->BackColor = System::Drawing::Color::White;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->ClientSize = System::Drawing::Size(456, 261);
+			this->Controls->Add(this->main_photo);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MaximumSize = System::Drawing::Size(472, 300);
+			this->MinimumSize = System::Drawing::Size(200, 100);
 			this->Name = L"main_win";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"main_win";
+			this->Text = L"VIPTOP";
 			this->Load += gcnew System::EventHandler(this, &main_win::main_win_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->main_photo))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void main_win_Load(System::Object^ sender, System::EventArgs^ e) {
 
+
 	}
-	};
+
+
+	private: System::Void main_photo_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+
+	}
+};
 }
